06a7e14c2d6e9bc3bfc1f502f3b5d0a0bdf2adfc
Yu Wang <ywmzn@amazon.com>
2020-03-16 09:03:55 -0700

    Use JacksonAnnotationIntrospector instead of Mixin to find builder class

--- costcategory/src/main/java/software/amazon/ce/costcategory/CostCategoryRulesParser.java
+++ costcategory/src/main/java/software/amazon/ce/costcategory/CostCategoryRulesParser.java
@@ -1,14 +1,20 @@
 package software.amazon.ce.costcategory;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.google.common.collect.ImmutableMap;
 import lombok.experimental.UtilityClass;
 import software.amazon.awssdk.services.costexplorer.model.*;
 import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Parser between JSON array string and list of {@link CostCategoryRule}.
@@ -32,13 +38,25 @@ public class CostCategoryRulesParser {
         OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 
         // SDK model has no default constructor, and build class is private.
-        // Mixin custom builder so Jackson can deserialize the class.
-        OBJECT_MAPPER.addMixIn(CostCategoryRule.class, CostCategoryRuleBuilder.class);
-        OBJECT_MAPPER.addMixIn(Expression.class, ExpressionBuilder.class);
-        OBJECT_MAPPER.addMixIn(DimensionValues.class, DimensionValuesBuilder.class);
-        OBJECT_MAPPER.addMixIn(TagValues.class, TagValuesBuilder.class);
-        OBJECT_MAPPER.addMixIn(CostCategoryValues.class, CostCategoryValuesBuilder.class);
+        // Add custom builder so Jackson can deserialize the class.
+        final Map<Class<?>, Class<?>> buildersMap = ImmutableMap.of(
+                CostCategoryRule.class, CostCategoryRule.serializableBuilderClass(),
+                Expression.class, Expression.serializableBuilderClass(),
+                DimensionValues.class, DimensionValues.serializableBuilderClass(),
+                TagValues.class, TagValues.serializableBuilderClass(),
+                CostCategoryValues.class, CostCategoryValues.serializableBuilderClass()
+        );
+        OBJECT_MAPPER.setAnnotationIntrospector(new JacksonAnnotationIntrospector() {
+            private static final long serialVersionUID = 1L;
 
+            @Override
+            public Class<?> findPOJOBuilder(AnnotatedClass ac) {
+                if (buildersMap.containsKey(ac.getRawType())) {
+                    return buildersMap.get(ac.getRawType());
+                }
+                return super.findPOJOBuilder(ac);
+            }
+        });
         objectWriter = OBJECT_MAPPER.writerWithDefaultPrettyPrinter();
     }
 
@@ -59,83 +77,6 @@ public class CostCategoryRulesParser {
             return objectWriter.writeValueAsString(rules);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    @JsonDeserialize(builder = CostCategoryRuleBuilder.class)
-    static class CostCategoryRuleBuilder {
-        private String value;
-        private Expression rule;
-
-        public CostCategoryRule build() {
-            return CostCategoryRule.builder()
-                    .value(value)
-                    .rule(rule)
-                    .build();
-        }
-    }
-
-    @JsonDeserialize(builder = ExpressionBuilder.class)
-    static class ExpressionBuilder {
-        private Collection<Expression> and;
-        private Collection<Expression> or;
-        private Expression not;
-        private DimensionValues dimensions;
-        private TagValues tags;
-        private CostCategoryValues costCategories;
-
-        public Expression build() {
-            return Expression.builder()
-                    .and(and)
-                    .or(or)
-                    .not(not)
-                    .dimensions(dimensions)
-                    .tags(tags)
-                    .costCategories(costCategories)
-                    .build();
-        }
-    }
-
-    @JsonDeserialize(builder = DimensionValuesBuilder.class)
-    static class DimensionValuesBuilder {
-        private String key;
-        private Collection<String> values;
-        private Collection<MatchOption> matchOptions;
-
-        public DimensionValues build() {
-            return DimensionValues.builder()
-                    .key(key)
-                    .values(values)
-                    .matchOptions(matchOptions)
-                    .build();
-        }
-    }
-
-    @JsonDeserialize(builder = TagValuesBuilder.class)
-    static class TagValuesBuilder {
-        private String key;
-        private Collection<String> values;
-        private Collection<MatchOption> matchOptions;
-
-        public TagValues build() {
-            return TagValues.builder()
-                    .key(key)
-                    .values(values)
-                    .matchOptions(matchOptions)
-                    .build();
-        }
-    }
-
-    @JsonDeserialize(builder = CostCategoryValuesBuilder.class)
-    static class CostCategoryValuesBuilder {
-        private String key;
-        private Collection<String> values;
-
-        public CostCategoryValues build() {
-            return CostCategoryValues.builder()
-                    .key(key)
-                    .values(values)
-                    .build();
         }
     }
 }
