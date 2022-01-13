d83d86bfd56a4efd42952d45a32b6451dd7163cc
Tobias Stodieck <tstodiec@amazon.com>
2022-01-13 17:07:03 -0500

    uncomment system tag functionality

--- anomalysubscription/src/main/java/software/amazon/ce/anomalysubscription/TagHelper.java
+++ anomalysubscription/src/main/java/software/amazon/ce/anomalysubscription/TagHelper.java
@@ -1,40 +1,13 @@
 package software.amazon.ce.anomalysubscription;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import org.apache.commons.collections.CollectionUtils;
-import software.amazon.awssdk.services.costexplorer.model.ResourceTag;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
 
 public class TagHelper {
-    /**
-     * convertToMap
-     *
-     * Converts a collection of Tag objects to a tag-name -> tag-value map.
-     *
-     * Note: Tag objects with null tag values will not be included in the output
-     * map.
-     *
-     * @param tags Collection of tags to convert
-     * @return Converted Map of tags
-     */
-    public static Map<String, String> convertToMap(final Collection<ResourceTag> tags) {
-        if (CollectionUtils.isEmpty(tags)) {
-            return Collections.emptyMap();
-        }
-        return tags.stream()
-                .filter(tag -> tag.value() != null)
-                .collect(Collectors.toMap(
-                        ResourceTag::key,
-                        ResourceTag::value,
-                        (oldValue, newValue) -> newValue));
-    }
-
     /**
      * generateTagsForCreate
      *
@@ -44,11 +17,9 @@ public class TagHelper {
     public static Map<String, String> generateTagsForCreate(final ResourceHandlerRequest<ResourceModel> handlerRequest) {
         final Map<String, String> tagMap = new HashMap<>();
 
-        /* TODO Uncomment when system tags are enabled
         if (handlerRequest.getSystemTags() != null) {
             tagMap.putAll(handlerRequest.getSystemTags());
         }
-         */
 
         if (handlerRequest.getDesiredResourceTags() != null) {
             tagMap.putAll(handlerRequest.getDesiredResourceTags());
