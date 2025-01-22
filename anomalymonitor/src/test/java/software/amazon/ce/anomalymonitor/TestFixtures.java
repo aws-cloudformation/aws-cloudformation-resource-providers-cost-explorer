afcebe96b857efa47275cba54dcb983752c76be0
Keaton Mueller <keatomue@amazon.com>
2025-01-22 14:38:45 -0500

    resolve small spotbugs errors

--- anomalymonitor/src/test/java/software/amazon/ce/anomalymonitor/TestFixtures.java
+++ anomalymonitor/src/test/java/software/amazon/ce/anomalymonitor/TestFixtures.java
@@ -6,22 +6,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TestFixtures {
-    public static String MONITOR_TYPE_CUSTOM = "CUSTOM";
-    public static String MONITOR_TYPE_DIMENSIONAL = "DIMENSIONAL";
-    public static String MONITOR_NAME = "TestMonitorName";
-    public static String TAGS_MONITOR_SPEC = "{\"Tags\":{\"Key\":\"aws:createdBy\",\"Values\":[\"TagValue1\",\"TagValue2\"],\"MatchOptions\":null}}";
-    public static String CC_MONITOR_SPEC = "{\"Or\":null,\"And\":null,\"Not\":null,\"Dimensions\":null,\"Tags\":null,\"CostCategories\":{\"Key\":\"myResourceEC2Category\",\"Values\":[\"EC2\"]}}";
-    public static String LINKEDACCOUNT_MONITOR_SPEC = "{\"Or\":null,\"And\":null,\"Not\":null,\"Dimensions\":{\"Key\":\"LINKED_ACCOUNT\",\"Values\":[\"123456789012\"],\"MatchOptions\":null},\"Tags\":null,\"CostCategories\":null}";
-    public static String INVALID_TYPE_MONITOR_SPEC = "Invalid";
-    public static String INVALID_VALUE_MONITOR_SPEC = "{\"Invalid\":{}}";;
-    public static String MONITOR_ARN = "arn:aws:ce::123456789012:anomalymonitor/monitorId";
-    public static String NEXT_TOKEN = "NextToken";
-    public static String DATE = "2020-11-15T00:00::00Z";
-    public static String DIMENSION = "SERVICE";
-    public static int DIMENSIONAL_VALUE_COUNT = 100;
-    public static String RESOURCE_TAG_KEY = "TestResourceTagKey";
-    public static String RESOURCE_TAG_VALUE = "TestResourceTagValue";
-    public static List<ResourceTag> RESOURCE_TAGS = Arrays.asList(ResourceTag.builder()
+    public static final String MONITOR_TYPE_CUSTOM = "CUSTOM";
+    public static final String MONITOR_TYPE_DIMENSIONAL = "DIMENSIONAL";
+    public static final String MONITOR_NAME = "TestMonitorName";
+    public static final String TAGS_MONITOR_SPEC = "{\"Tags\":{\"Key\":\"aws:createdBy\",\"Values\":[\"TagValue1\",\"TagValue2\"],\"MatchOptions\":null}}";
+    public static final String CC_MONITOR_SPEC = "{\"Or\":null,\"And\":null,\"Not\":null,\"Dimensions\":null,\"Tags\":null,\"CostCategories\":{\"Key\":\"myResourceEC2Category\",\"Values\":[\"EC2\"]}}";
+    public static final String LINKEDACCOUNT_MONITOR_SPEC = "{\"Or\":null,\"And\":null,\"Not\":null,\"Dimensions\":{\"Key\":\"LINKED_ACCOUNT\",\"Values\":[\"123456789012\"],\"MatchOptions\":null},\"Tags\":null,\"CostCategories\":null}";
+    public static final String INVALID_TYPE_MONITOR_SPEC = "Invalid";
+    public static final String INVALID_VALUE_MONITOR_SPEC = "{\"Invalid\":{}}";;
+    public static final String MONITOR_ARN = "arn:aws:ce::123456789012:anomalymonitor/monitorId";
+    public static final String NEXT_TOKEN = "NextToken";
+    public static final String DATE = "2020-11-15T00:00::00Z";
+    public static final String DIMENSION = "SERVICE";
+    public static final int DIMENSIONAL_VALUE_COUNT = 100;
+    public static final String RESOURCE_TAG_KEY = "TestResourceTagKey";
+    public static final String RESOURCE_TAG_VALUE = "TestResourceTagValue";
+    static final List<ResourceTag> RESOURCE_TAGS = Arrays.asList(ResourceTag.builder()
             .key(RESOURCE_TAG_KEY)
             .value(RESOURCE_TAG_VALUE)
             .build()
